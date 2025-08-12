891ca31d792cc347a6ae6b5372e7736db5be321e
gemps <gempar.tri.asmara@gmail.com>
2025-08-13 00:01:55 +0700

    tesss

--- src/testsql.py
+++ src/testsql.py
@@ -3,3 +3,6 @@ import psycopg2
 def search_user(username):
     con=psycopg2.connect(dbname="testdb",user="testuser", password="testing", host="123.123.123.123", port=5432)
     query=f"select * from users where username='{username}'"
+    with con.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
